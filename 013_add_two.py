c2da7465aa4f480d98fd8fce83bb7dcc4bb3f30c
nikitar16 <130574339+nikitar16@users.noreply.github.com>
2023-09-14 14:20:21 +0100

    Update 013_add_two.py

--- 013_add_two.py
+++ 013_add_two.py
@@ -11,7 +11,9 @@ from lib.helpers import check_that_these_are_equal
 # * Returns the result
 
 # YOUR FUNCTION GOES BELOW THIS LINE
-
+def add_two(num)(
+  return num
+)
 
 
 # YOUR FUNCTION GOES ABOVE THIS LINE
